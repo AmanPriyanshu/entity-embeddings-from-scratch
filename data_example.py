c3f92448bd1558a9cf2700d5c1a8465d1f5b1ab2
Aman Priyanshu <aman.priyanshu@learner.manipal.edu>
2021-02-21 03:20:05 +0530

    up

---
+++ data_example.py
@@ -0,0 +1,71 @@
+import torch
+from embedding_layer import EntityEmbedding
+
+data = ['cat chases mice',
+		'cat catches mice',
+		'cat eats mice',
+		'mice runs into hole',
+		'cat says bad words',
+		'cat and mice are pals',
+		'cat and mice are chums',
+		'mice stores food in hole',
+		'cat stores food in house',
+		'mice sleeps in hole',
+		'cat sleeps in house',
+		'cat and mice are buddies',
+		'mice lives in hole',
+		'cat lives in house bruh ur mum']
+
+data = [i.lower() for i in data]
+vocab = list(set(' '.join(data).split()))
+
+word_key = {word:i+1 for i,word in enumerate(vocab)}
+
+def converter(s):
+	return [word_key[word] for word in s.split()]
+
+def pad_sequences(data, max_len=10):
+	max_len = max_len if max([len(i) for i in data])>max_len else max([len(i) for i in data])
+	return [i+[0]*(max_len-len(i)) if len(i)<max_len else i[:max_len] for i in data]
+
+class SimpleModel(torch.nn.Module):
+	def __init__(self, vocab_size, hidden_size, input_size):
+		super().__init__()
+		self.embedding_layer = EntityEmbedding(vocab_size, hidden_size, input_size)
+		self.hidden_size = hidden_size
+		self.vocab_size = vocab_size
+
+	def forward(self, x):
+		x = self.embedding_layer(x)
+		x = torch.nn.Linear(self.hidden_size, self.vocab_size)(x)
+		x = torch.nn.Softmax(dim=1)(x)
+		return x
+
+def generate_dataset(data, n=2):
+	x, y = [], []
+	for row in data:
+		for index in range(0, len(row)-n-1):
+			x.append(row[index:index+n])
+			y.append(row[index+n])
+	return torch.Tensor(x).int(), torch.Tensor(y).long()
+
+def train():
+	data_one_hot = [converter(i) for i in data]
+	data_padded = pad_sequences(data_one_hot)
+	x, y = generate_dataset(data_padded)
+	model = SimpleModel(vocab_size=len(vocab)+1, hidden_size=len(vocab)//4, input_size=2)
+
+	criterion = torch.nn.CrossEntropyLoss()
+	optimizer = torch.optim.Adam(model.parameters())
+
+	for epoch in range(500):
+		optimizer.zero_grad()
+		output = model(x)
+		loss = criterion(output, y)
+		print(loss)
+		loss.backward()
+		optimizer.step()
+
+if __name__ == '__main__':
+	train()
+	
--- embedding_layer.py
+++ embedding_layer.py
@@ -1,2 +1,29 @@
 import torch
+import numpy as np
 
+class EntityEmbedding(torch.nn.Module):
+	def __init__(self, vocab_size, hidden_size, input_size):
+		super(EntityEmbedding, self).__init__()
+		self.input_size = input_size
+		self.hidden_size = hidden_size
+		self.vocab_size = vocab_size
+		self.softmax = torch.nn.Softmax(dim=1)
+
+		weights = torch.Tensor(self.vocab_size, self.hidden_size)
+		self.weights = torch.nn.Parameter(weights)
+		torch.nn.init.kaiming_uniform_(self.weights, a=np.sqrt(5))
+
+	def forward(self, x):
+		x_onehot = torch.FloatTensor(x.shape[0], self.input_size, self.vocab_size)
+		x_onehot.zero_()
+		for i in range(x.shape[0]):
+			for row in range(x[i].shape[0]):
+				x_onehot[i][row][x[i][row]] += 1
+		w_times_x=torch.zeros(x.shape[0], self.input_size, self.hidden_size)
+		for i in range(x.shape[0]):
+			w_times_x[i] = torch.mm(x_onehot[i], self.weights)
+		return torch.mean(w_times_x, 1)
+
+if __name__ == '__main__':
+	layer = EntityEmbedding(10, 2, 3)
+	print(layer(torch.tensor([[0, 1, 2], [0, 1, 5]])))
